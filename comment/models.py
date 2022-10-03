e8f311fff4c63d46fc6f4ea4fec11168a2a1a0e1
BAGI IGR <bagi.igr@gmail.com>
2022-10-03 10:18:21 -0100

    backend

---
+++ comment/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fio = models.CharField(max_length=100, null=True, blank=True)
+    position = models.CharField(max_length=100, null=True, blank=True)
+    work_phone_num = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='./src/files/', null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_profile_and_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+        Profile.objects.create(user=instance)
+
+
+class Task(MPTTModel):
+    TASK_PRIORITY_CHOICE = (
+        ('Низкий', 'Низкий'),
+        ('Средний', 'Средний'),
+        ('Высокий', 'Высокий'),
+    )
+
+    TASK_STATUS_CHOICE = (
+        ('Открыта', 'Открыта'),
+        ('В работе', 'В работе'),
+        ('Закрыта', 'Закрыта'),
+    )
+
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='tasks')
+    assignee = models.ManyToManyField(Profile)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    deadline = models.DateField()
+    is_active = models.BooleanField(default=True)
+    status = models.CharField(max_length=100, choices=TASK_STATUS_CHOICE, default='Открыта')
+    priority = models.CharField(max_length=100, choices=TASK_PRIORITY_CHOICE, default='low')
+    file = models.FileField(verbose_name='files/', null=True)
+
+    def __str__(self):
+        return self.title
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Report(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='author')
+    assignee = models.ManyToManyField(Profile)
+    content = models.TextField(blank=True)
+    task = models.OneToOneField(Task, on_delete=models.CASCADE, null=True, blank=True, related_name='report')
+    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(verbose_name='files/', null=True)
+
+    def __str__(self):
+        return self.content
---
+++ config/settings.py
@@ -0,0 +1,121 @@
+import os
+from pathlib import Path
+import django_heroku
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'comment',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'django.contrib.sites',
+    'corsheaders',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'build')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SITE_ID = 1
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
+
+DJOSER = {
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SERIALIZERS': {},
+}
+
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+CORS_ALLOW_ALL_ORIGINS = True
+
+django_heroku.settings(locals())
