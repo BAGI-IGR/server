82e981d7785f728757cb47426314e4386e747bd0
BAGI IGR <bagi.igr@gmail.com>
2023-01-24 15:09:34 +0300

    add progress

--- comment/serializer.py
+++ comment/serializer.py
@@ -37,5 +37,5 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ('id', 'author', 'assignee', 'title', 'description',
-                  'parent', 'created_at', 'update_at', 'deadline',
+                  'parent', 'created_at', 'update_at', 'deadline', 'progress',
                   'is_active', 'status', 'priority', 'file', 'comments', 'report')
