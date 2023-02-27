a08e395f9ff19ed15de098bef4a7e48c2338d4b0
BAGI IGR <bagi.igr@gmail.com>
2023-02-27 14:04:23 +0300

    rework task view

--- src/Task_View.js
+++ src/Task_View.js
@@ -71,56 +71,93 @@ export default function View() {
         }
     }
     return (
-        <div className="all">
-            <div className="one">
-                <div className="task-name">
-                    <span className="task-name__style">
-                        {title}
-                    </span>
-                </div>
-                <div className="task-description">
-                    <span className="task-name__style">{description}</span>
-                </div>
-                <div className="edit">
-                    {localStorage.getItem('user_id') == 2 && <a className="edit_button" href={generatePath("/task-update/:id/", {id: localStorage.getItem('id')})}><span>Редактировать</span></a>}
-                </div>
-                <div className="comment">
-                    <div>
-                        <form id={'submit_comment'} onSubmit={Comment}>
-                            <input type="text" className="task_1" name={'text'}  placeholder="Введите комментарий"/>
-                            <button id={'fake_knopka'} className={'fake_knopka'}/>
-                        </form>
-                    </div>
-                    <div className={'chat'}>
-                        {comments.map(comment => (
-                        <div className="task_1">
-                            {comment.text}
-                        </div>
-                        ))}
-                    </div>
-                </div>
+        // <div className="all">
+        //     <div className="one">
+        //         <div className="task-name">
+        //             <span className="task-name__style">
+        //                 {title}
+        //             </span>
+        //         </div>
+        //         <div className="task-description">
+        //             <span className="task-name__style">{description}</span>
+        //         </div>
+        //         <div className="edit">
+        //             {localStorage.getItem('user_id') == 2 && <a className="edit_button" href={generatePath("/task-update/:id/", {id: localStorage.getItem('id')})}><span>Редактировать</span></a>}
+        //         </div>
+        //         <div className="comment">
+        //             <div>
+        //                 <form id={'submit_comment'} onSubmit={Comment}>
+        //                     <input type="text" className="task_1" name={'text'}  placeholder="Введите комментарий"/>
+        //                     <button id={'fake_knopka'} className={'fake_knopka'}/>
+        //                 </form>
+        //             </div>
+        //             <div className={'chat'}>
+        //                 {comments.map(comment => (
+        //                 <div className="task_1">
+        //                     {comment.text}
+        //                 </div>
+        //                 ))}
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <div сlassName="all_2">
+        //         <div className="three">
+        //             <div className="Intelligence">
+        //                 <span className="task-name__style">Сведения</span>
+        //             </div>
+        //             <div className="Executor">
+        //                 <span className="task-name__style">Исполнитель: {localStorage.getItem('users' + (assignee[0] - 1))}</span>
+        //             </div>
+        //             <div className="Author">
+        //                 <span className="task-name__style">Автор: {localStorage.getItem('users' + (author - 1))}</span>
+        //                 </div>
+        //             <div className="Priority">
+        //                 <span className="task-name__style">Приоритет: {priority}</span>
+        //             </div>
+        //             <div className="Created">
+        //                 <span className="task-name__style">Создано:</span><input className="date" value={created_at} type="date"/>
+        //             </div>
+        //             <div className="dedline">
+        //                 <span className="task-name__style">Дедлайн:</span><input className="date" value={deadline} type="date"/>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <div>
+            <div className="footer">
+                <h2 className="textbox">Название задачи</h2>
             </div>
-            <div сlassName="all_2">
-                <div className="three">
-                    <div className="Intelligence">
-                        <span className="task-name__style">Сведения</span>
-                    </div>
-                    <div className="Executor">
-                        <span className="task-name__style">Исполнитель: {localStorage.getItem('users' + (assignee[0] - 1))}</span>
-                    </div>
-                    <div className="Author">
-                        <span className="task-name__style">Автор: {localStorage.getItem('users' + (author - 1))}</span>
-                        </div>
-                    <div className="Priority">
-                        <span className="task-name__style">Приоритет: {priority}</span>
-                    </div>
-                    <div className="Created">
-                        <span className="task-name__style">Создано:</span><input className="date" value={created_at} type="date"/>
-                    </div>
-                    <div className="dedline">
-                        <span className="task-name__style">Дедлайн:</span><input className="date" value={deadline} type="date"/>
-                    </div>
+            <div className="status">
+                <select className="task-status">
+                    <option value="1">В работе</option>
+                    <option value="2">Завершено</option>
+                    <option value="3">Не начато</option>
+                </select>
+                <p><input type="button" value="Дедлайн" className="deadline"/></p>
+            </div>
+            <div className="create-task">
+                <input className="name_task" placeholder={title}/>
+                <input className="description" placeholder={description}/>
+                <div>
+                    <div className="executor">Исполнитель :</div>
+                    <input type="text" className="fio" placeholder={localStorage.getItem('users' + (assignee[0] - 1))}/>
+                    <button type="submit" className="add_fio"/>
                 </div>
+                <button type="submit" className="add_file">+ Добавить файл</button>
+                <div className="attached_files">Вложенные файлы
+                    <button type="submit" className="add_1"></button>
+                    <button type="submit" className="add_2"></button>
+                    <button type="submit" className="add_3"></button>
+                </div>
+                <button type="submit" className="save_task">Сохранить и закрыть</button>
+                <button type="submit" className="delete_task">Удалить задачу</button>
+            </div>
+            <div className="chat"/>
+            <div className="message">
+                <input type="text" className="task_1" name="text" placeholder="Введите комментарий"/>
+                <span className="send">
+                    <a className="send_message">Отправить</a>
+                </span>
             </div>
         </div>
     )
